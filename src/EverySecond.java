de71911674bf45fc105b7ceb6d76af43dafb71a5
Marten Hernebring <marten.hernebring@gmail.com>
2021-08-31 14:30:12 +0200

    Bug start b4 end

--- src/EverySecond.java
+++ src/EverySecond.java
@@ -9,22 +9,22 @@ public class EverySecond {
         Instant instant = Instant.now().minus(1000, ChronoUnit.SECONDS);
         Instant secondAfter = instant.plus(1, ChronoUnit.SECONDS);
         final int STEPS = 2;
-        String endTime = instant.toString().substring(0,23);
+        String endTime = secondAfter.toString().substring(0,23);
         try(BufferedWriter bw = new BufferedWriter(new FileWriter("seconds.txt"))){
             bw.write("[");
             bw.write("\n  {\n" +
                     "    \"endTime\": \""+ endTime +"Z\",\n" +
-                    "    \"startTime\": \""+secondAfter.toString().substring(0,23)+"Z\",\n" +
+                    "    \"startTime\": \""+instant.toString().substring(0,23)+"Z\",\n" +
                     "    \"stepCount\": "+STEPS+",\n" +
                     "    \"uploadedTime\": \""+ endTime +"Z\"\n" +
                     "  }");
             for(int i = 1; i<1000; i++){
                 instant = secondAfter;
                 secondAfter = instant.plus(1, ChronoUnit.SECONDS);
-                endTime = instant.toString().substring(0,23);
+                endTime = secondAfter.toString().substring(0,23);
                 bw.write(",\n  {\n" +
                         "    \"endTime\": \""+ endTime +"Z\",\n" +
-                        "    \"startTime\": \""+secondAfter.toString().substring(0,23)+"Z\",\n" +
+                        "    \"startTime\": \""+instant.toString().substring(0,23)+"Z\",\n" +
                         "    \"stepCount\": "+STEPS+",\n" +
                         "    \"uploadedTime\": \""+ endTime +"Z\"\n" +
                         "  }");
